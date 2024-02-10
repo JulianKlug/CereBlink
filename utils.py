a6332c29c6b8ca274236f9f209f5b1469c02e707
Julian Klug <tensu.wave@gmail.com>
2024-02-10 20:17:13 +0100

    Check distribution of DCI timing

--- utils.py
+++ utils.py
@@ -1,3 +1,32 @@
+import pandas as pd
+import io
+import getpass
+import msoffcrypto
+
+
+def load_encrypted_xlsx(file_path, sheet_name=None):
+    """
+        Loads an encrypted Excel file and returns the data as a pandas DataFrame.
+
+        Args:
+            file_path (str): The path to the encrypted Excel file.
+            sheet_name (str, optional): The name of the sheet to load. Defaults to None.
+
+        Returns:
+            pandas.DataFrame: The data from the specified sheet as a DataFrame.
+    """
+    password = getpass.getpass()
+    decrypted_workbook = io.BytesIO()
+    with open(file_path, 'rb') as file:
+        office_file = msoffcrypto.OfficeFile(file)
+        office_file.load_key(password=password)
+        office_file.decrypt(decrypted_workbook)
+
+    if sheet_name is None:
+        df = pd.read_excel(decrypted_workbook)
+    else:
+        df = pd.read_excel(decrypted_workbook, sheet_name=sheet_name)
+    return df
 
 
 def flatten(l):
